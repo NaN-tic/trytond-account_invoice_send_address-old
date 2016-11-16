564d9be7a93f560eb1ce47620a55a6b339bf6723
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-11-16 16:21:36 +0100

    Use doctest checker for python3 compatibility

--- tests/test_account_invoice_send_address.py
+++ tests/test_account_invoice_send_address.py
@@ -5,6 +5,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class TestCase(ModuleTestCase):
@@ -17,9 +18,11 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_invoice_send_address.rst',
             tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_invoice_send_address_sale.rst',
             tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
