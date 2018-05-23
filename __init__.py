d5f6ec4593ad7b4d6eb6a9ec790b3088a4b1f55c
Raimon Esteve <raimon@nan-tic.com>
2018-05-24 01:01:08 +0200

    Upgrade to 4.8

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import  *
+from . import invoice
 
 
 def register():
     Pool.register(
-        Address,
-        Invoice,
-        Sale,
+        invoice.Address,
+        invoice.Invoice,
+        module='account_invoice_send_address', type_='model')
+    Pool.register(
+        invoice.Sale,
+        depends=['sale'],
         module='account_invoice_send_address', type_='model')
